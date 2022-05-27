449b0e33e2e4a7bc5500fa4c5e95c2de43f980d8
tiagofernandes27 <78868193+tiagofernandes27@users.noreply.github.com>
2022-05-27 14:30:48 +0100

    Buttons in the middle of the screeeeeen!!

--- Project/Project/Game1.cs
+++ Project/Project/Game1.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Project.Controls2;
 using Project.Models;
 using Project.Sprites;
+using System;
 using System.Collections.Generic;
 
 namespace Project
@@ -19,6 +21,10 @@ namespace Project
 
         private List<Sprite> _sprites;
 
+        //Button
+        private Color _backgroundColour = Color.CornflowerBlue;
+
+        private List<Component> _gameComponents;
 
         public Game1()
         {
@@ -42,7 +48,29 @@ namespace Project
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            #region Buttons
+            var randomButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(350, 200),
+                Text="Random",
+            };
+
+            randomButton.Click += RandomButton_Click;
+
+            var quitButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(350, 250),
+                Text = "Quit",
+            };
+
+            quitButton.Click += QuitButton_Click;
+
+            _gameComponents = new List<Component>
+            {
+                randomButton,
+                quitButton,
+            };
+            #endregion
 
             var playerTexture = Content.Load<Texture2D>("Ball");
 
@@ -81,9 +109,24 @@ namespace Project
             };
         }
 
+        private void QuitButton_Click(object sender, System.EventArgs e)
+        {
+            Exit();
+        }
+
+        private void RandomButton_Click(object sender, System.EventArgs e)
+        {
+            var random = new Random();
+
+            _backgroundColour = new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+        }
+
         protected override void Update(GameTime gameTime)
         {
-
+            foreach(var component in _gameComponents)
+            {
+                component.Update(gameTime);
+            }
             // TODO: Add your update logic here
 
             foreach (var sprite in _sprites)
@@ -97,11 +140,14 @@ namespace Project
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(_backgroundColour);
 
             // TODO: Add your drawing code here
 
             _spriteBatch.Begin();
+
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, _spriteBatch);
 
             foreach (var sprite in _sprites)
                 sprite.Draw(_spriteBatch);
