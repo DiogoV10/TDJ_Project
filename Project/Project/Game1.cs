221fa1af6f8cf60d3f8bda5eaba33258f31cebe6
tiagofernandes27 <78868193+tiagofernandes27@users.noreply.github.com>
2022-05-27 15:53:57 +0100

    Buttons & States

--- Project/Project/Game1.cs
+++ Project/Project/Game1.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using Project.Controls2;
 using Project.Models;
 using Project.Sprites;
+using Project.States;
 using System;
 using System.Collections.Generic;
 
@@ -14,17 +15,24 @@ namespace Project
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //Screen Measures
         public static int ScreenWidth;
         public static int ScreenHeight;
 
+        //KeyboardManager
         private KeyboardManager km;
 
+        //Sprites
         private List<Sprite> _sprites;
 
-        //Button
-        private Color _backgroundColour = Color.CornflowerBlue;
+        //States
+        private State _currentState;
+        private State _nextState;
+        public void ChangeState(State state)
+        {
+            _nextState = state;
+        }
 
-        private List<Component> _gameComponents;
 
         public Game1()
         {
@@ -48,29 +56,7 @@ namespace Project
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            #region Buttons
-            var randomButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
-            {
-                Position = new Vector2(350, 200),
-                Text="Random",
-            };
-
-            randomButton.Click += RandomButton_Click;
-
-            var quitButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
-            {
-                Position = new Vector2(350, 250),
-                Text = "Quit",
-            };
-
-            quitButton.Click += QuitButton_Click;
-
-            _gameComponents = new List<Component>
-            {
-                randomButton,
-                quitButton,
-            };
-            #endregion
+            _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
 
             var playerTexture = Content.Load<Texture2D>("Ball");
 
@@ -109,25 +95,18 @@ namespace Project
             };
         }
 
-        private void QuitButton_Click(object sender, System.EventArgs e)
-        {
-            Exit();
-        }
-
-        private void RandomButton_Click(object sender, System.EventArgs e)
-        {
-            var random = new Random();
-
-            _backgroundColour = new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
-        }
 
         protected override void Update(GameTime gameTime)
         {
-            foreach(var component in _gameComponents)
+            if(_nextState != null)
             {
-                component.Update(gameTime);
+                _currentState = _nextState;
+                _nextState = null;
             }
-            // TODO: Add your update logic here
+
+            _currentState.Update(gameTime);
+
+            _currentState.PostUpdate(gameTime);
 
             foreach (var sprite in _sprites)
             {
@@ -140,14 +119,12 @@ namespace Project
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(_backgroundColour);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
 
-            foreach (var component in _gameComponents)
-                component.Draw(gameTime, _spriteBatch);
+            _currentState.Draw(gameTime, _spriteBatch);
+
 
             foreach (var sprite in _sprites)
                 sprite.Draw(_spriteBatch);
